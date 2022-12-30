68e020a521c1f90e448a58ab5121d38de1ef6e3e
eastcoastdeveloper <uxdeveloper@protonmail.com>
2022-12-30 15:32:08 -0800

    notify Angular of the interceptor

--- app-ui/src/app/app.module.ts
+++ app-ui/src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./components/home/home.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { LoadingInterceptor } from "./guards/loading.interceptor";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, HeaderComponent],
   imports: [HttpClientModule, BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [
+    HttpClientModule,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
