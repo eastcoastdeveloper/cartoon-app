a4c256098d6bd862c8f542f1262cc17451486bda
eastcoastdeveloper <uxdeveloper@protonmail.com>
2022-12-30 15:31:09 -0800

    move all files to new directory

--- src/app/interfaces/dummyData.ts
+++
@@ -1,9 +0,0 @@
-// Representation of the Data Object
-export class DummyDataInterface {
-  caption?: string;
-  firstName?: string;
-  lastName?: string;
-  city?: string;
-  state?: string;
-  country?: string;
-}
