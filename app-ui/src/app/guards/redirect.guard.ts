d2f16188812778615307c6bca12be3fc4b9cb81a
eastcoastdeveloper <uxdeveloper@protonmail.com>
2022-12-30 15:18:23 -0800

    intercept requests to display loader during fetching

---
+++ app-ui/src/app/guards/redirect.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from "@angular/router";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class RedirectGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    window.location.href = route.data["externalUrl"];
+    return true;
+  }
+}
