db3b4fdacce9da6325cfe58f877c1633751f4804
eastcoastdeveloper <uxdeveloper@protonmail.com>
2023-01-06 23:09:16 -0800

    add Helmet for backend security

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 const port = process.env.PORT || 8080;
+
+app.use(helmet());
 
 // Get Captions
 app.use('/', require('./endpoints/submission-data'))
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+app.listen(port, (err) => {
+  if (err) { console.log(err); }
+  else {
+    console.log(`listening on port ${port}`);
+  }
 })
