08d72a7a2947a9a15d43954f67ad2149beab4cbe
eastcoastdeveloper <uxdeveloper@protonmail.com>
2023-01-23 09:43:04 -0800

    zebra stripe captions for better readability

--- api/routes/approved-captions.js
+++ api/routes/approved-captions.js
@@ -3,33 +3,21 @@ const router = express.Router();
 const projectData = require('../captions-data.json');
 
 function populateUI(model) {
-      return (req, res, next) => {
-        const page = parseInt(req.query.page);
-        const limit = parseInt(req.query.limit);
-        const startIndex = (page - 1) * limit;
-        const endIndex = page * limit;
-        const results = {};
-    
-        results.results = model.captions.slice(startIndex, endIndex)
+  return (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const results = {};
 
-        res.populateUI = results;
-        console.log(results)
-        console.log(page)
-        console.log(limit)
-        next();
-      }
-  
-      // return (req, res, next) => {
-      //   const page = parseInt(req.query.page);
-      //   const limit = parseInt(req.query.limit);
-      //   const startIndex = (page - 1) * limit;
-      //   const endIndex = page * limit;
-      //   const results = {};
-    
-      //   results.results = model.captions.slice(startIndex, endIndex);
-      //   res.paginatedResults = results;
-      //   next();
-      // }
+    results.results = model.captions.slice(startIndex, endIndex)
+
+    res.populateUI = results;
+    console.log(results)
+    console.log(page)
+    console.log(limit)
+    next();
+  }
 }
 
 router.get('/', populateUI(projectData), (req, res) => {
